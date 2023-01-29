430f97126c6b3964d082f231abc65088fdff6fcb
Albert Delgado M <Albert Delgado M@DESKTOP-4858LBT>
2023-01-28 21:10:51 -0400

    Add project files.

---
+++ DAL/Contexto.cs
@@ -0,0 +1,10 @@
+using Microsoft.EntityFrameworkCore;
+
+public class Contexto : DbContext{
+
+    public DbSet<Ocupaciones> Ocupaciones {get;set;}
+    public Contexto(DbContextOptions <Contexto> options): base(options)
+    {
+    }
+
+}
